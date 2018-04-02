bc872938f7ca91431d50738be5c1fe87e8d49f52
Daniel Brain <dbrain@paypal.com>
2018-04-02 14:27:15 -0700

    Use grumbler eslintrc-node

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,5 @@
 /* @flow */
 
 module.exports = {
-    'extends': './node_modules/grumbler-scripts/config/.eslintrc.js',
-
-    rules: {
-        'import/no-dynamic-require': 'off',
-        'no-restricted-globals': 'off',
-        'promise/no-native': 'off',
-        'import/no-nodejs-modules': 'off',
-        'compat/compat': 'off',
-        'no-process-env': 'off'
-    }
+    'extends': './node_modules/grumbler-scripts/config/.eslintrc-node.js'
 };
