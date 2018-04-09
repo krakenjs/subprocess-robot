15c1a2f092b066b9560a39ee9e4e438377d49182
Daniel Brain <dbrain@paypal.com>
2018-04-09 15:16:25 -0700

    Do not sleep processes in CI env

--- test/exports.js
+++ test/exports.js
@@ -5,6 +5,11 @@ export function multiply(n : number, m : number) : number {
 }
 
 export function sleep(time : number) {
+
+    if (process.env.TRAVIS) {
+        return;
+    }
+
     let start = Date.now();
 
     while ((Date.now() - start) < time) {
